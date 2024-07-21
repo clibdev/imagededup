b8c430a45f49084e15c8843c14502131f983a13e
clibdev <clibdev@gmail.com>
2024-07-21 19:05:05 +0300

    update demo

--- demo.py
+++ demo.py
@@ -1,12 +1,13 @@
 from imagededup.methods import CNN
 
-img_dir='tests/data/mixed_images'
+img_dir = 'tests/data/mixed_images'
 
 cnn = CNN()
-duplicates = cnn.find_duplicates(image_dir=img_dir, scores=True, min_similarity_threshold=0.9)
+duplicates = cnn.find_duplicates(image_dir=img_dir, scores=True, min_similarity_threshold=0.95)
 
 for img in duplicates:
-    print(img + ':')
-    for duplicate in duplicates[img]:
-        print(duplicate[0] + ' (' + str(duplicate[1]) + ')')
-    print('------------')
+    if duplicates[img]:
+        print(img + ':')
+        for duplicate in duplicates[img]:
+            print(duplicate[0] + ' (' + str(duplicate[1]) + ')')
+        print('------------')
